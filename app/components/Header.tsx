8ed5922a2d44b7af5b06863aae86f76bd95c30f3
Luke Irwin <lukemirwin@gmail.com>
2023-09-13 08:47:21 -0500

    mobile reformat

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,28 +4,6 @@ import Image from "next/image";
 import icon from "../../e-pacerr_icon.png";
 import Sidenav from "./Sidenav"
 
-// function Header() {
-//   return (
-//     <header className="header">
-//       <div className="logo-container">
-//         <a href="/"><Image src={icon} alt="icon" className="icon"></Image></a>
-//       </div>
-//       <div className="link-container">
-//         <a href="/" className="nav-link">Home</a>
-//         <a href="/modules" className="nav-link">Modules</a>
-//         <a href="/faq" className="nav-link">FAQs</a>
-//         <button className="click"><a href="/enroll" className="button-text">Enroll Now!</a></button>
-//       </div>
-//       <div className="spacer"></div>
-//       <Sidenav />
-//     </header>
-
-//   );
-// }
-
-// export default Header;
-
-
 const Header = () => {
   const [isLargerScreen, setIsLargeScreen] = useState(false);
   
